813835f98e6acac31c7bb283822a421168ec9a9d
Noah Watkins <noahwatkins@gmail.com>
2014-11-08 13:50:22 -0800

    test: cleanup and tune for fast/slow mode
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import random
 import fractions
 import unittest
 import cffi
+import os
 
 ffi = cffi.FFI()
 libbeal = ffi.dlopen("./libbeal.so")
@@ -18,6 +19,11 @@ void axby_next(void *axbyp, struct point *pp, int count);
 void axby_free(void *axbyp);
 ''')
 
+# run fast tests?
+_FAST = False
+if os.environ.get("FAST") == "1":
+    _FAST = True
+
 class TestModPow(unittest.TestCase):
     def __check(self, b, e, m):
         value1 = pow(b, e, m)
@@ -26,13 +32,17 @@ class TestModPow(unittest.TestCase):
                 "%d != %d: %d %d %d" % (value1, value2, b, e, m))
 
     def test_dense(self):
-        for base in range(1, 200):
-            for expo in range(1, 200):
-                for mod in range(1, 200):
+        # 2,3,4,5x100 -> 3s,12s,30s,1m
+        limit = 200 if _FAST else 500
+        for base in range(1, limit):
+            for expo in range(1, limit):
+                for mod in range(1, limit):
                     self.__check(base, expo, mod)
 
     def test_random(self):
-        for _ in range(10000):
+        # 10K,1M,10M -> .5s,11s,2m
+        limit = 10**4 if _FAST else 10**7
+        for _ in range(limit):
             base = random.randint(1, 2**64-1)
             expo = random.randint(1, 2**64-1)
             mod = random.randint(1, 2**32-1)
@@ -52,18 +62,22 @@ class TestGCD(unittest.TestCase):
         self.assertEqual(value1, value2, "%u %u" % (u, v))
 
     def test_dense(self):
-        for u in range(1, 100):
-            for v in range(1, 100):
+        # 1K,10K -> .5s,40s
+        limit = 10**3 if _FAST else 10**4
+        for u in xrange(1, limit):
+            for v in xrange(1, limit):
                 self.__check(u, v)
 
     def test_random(self):
-        for _ in range(1000):
+        # 1M,10M,100M -> 1.2s,10s,1m40s
+        limit = 10**6 if _FAST else 10**8
+        for _ in xrange(limit):
             u = random.randint(1, 2**32-1)
             v = random.randint(1, 2**32-1)
             self.__check(u, v)
 
 class TestCz(unittest.TestCase):
-    def __check(self, maxb, maxp, mod):
+    def __check(self, maxb, maxp, mod, fast=False):
         czp = libbeal.cz_make(maxb, maxp, mod)
 
         values = set()
@@ -73,8 +87,9 @@ class TestCz(unittest.TestCase):
                 value2 = libbeal.cz_get(czp, c, z)
                 self.assertEqual(value1, value2)
                 values.add(value2)
+        assert len(values) > 0
 
-        # assert that ever value in c^z is in the exists set
+        # assert that every value in c^z is set in the exists set
         for value in values:
             exists = libbeal.cz_exists(czp, value)
             self.assertTrue(exists)
@@ -82,7 +97,9 @@ class TestCz(unittest.TestCase):
         # iterating over all 2^32 positions in the exists set is expensive so
         # we just perform a random sampling. anything that isn't false should
         # be in the exists set we calculated above from the cz table
-        for _ in xrange(10000):
+        # 10M,100M -> 1.3s,12s,
+        limit = 10**6 if fast else 10**8
+        for _ in xrange(limit):
             value = random.randint(0, 2**32-1)
             exists = libbeal.cz_exists(czp, value)
             if exists:
@@ -91,21 +108,27 @@ class TestCz(unittest.TestCase):
         libbeal.cz_free(czp)
 
     def test_random(self):
-        for _ in xrange(10):
-            maxb = random.randint(1, 100)
-            maxp = random.randint(3, 100)
+        limit = 2 if _FAST else 40
+        for _ in xrange(limit):
+            maxb = random.randint(1, 2000)
+            maxp = random.randint(3, 2000)
             mod = random.randint(1, 2**32-1)
-            self.__check(maxb, maxp, mod)
+            self.__check(maxb, maxp, mod, True)
 
     def test_specific(self):
         self.__check(1000, 1000, 4294967291)
         self.__check(1000, 1000, 4294967279)
+        self.__check(2000, 2000, 4294967279)
+        self.__check(2000, 2000, 2**32-1)
 
     def test_dense(self):
-        for maxb in range(1, 10):
-            for maxp in range(3, 10):
-                for mod in range(1, 10):
-                    self.__check(maxb, maxp, mod)
+        for maxb in range(1, 12):
+            for maxp in range(3, 12):
+                self.__check(maxb, maxp, 4294967291, True)
+        for maxb in range(1, 12):
+            self.__check(maxb, 100000, 4294967291, True)
+        for maxp in range(3, 14):
+            self.__check(100000, maxp, 4294967291, True)
 
 class TestAxby(unittest.TestCase):
     def test_all_points(self):
