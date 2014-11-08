12ea4aaf05c55313dee547df73734a2709470cb4
Noah Watkins <noahwatkins@gmail.com>
2014-11-07 20:09:05 -0800

    test: add beal helper for cffi stuff
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- test.py
+++ test.py
@@ -1,23 +1,8 @@
 import random
 import fractions
 import unittest
-import cffi
 import os
-
-ffi = cffi.FFI()
-libbeal = ffi.dlopen("./libbeal.so")
-ffi.cdef('''
-uint32_t c_modpow(uint64_t base, uint64_t exponent, uint32_t mod);
-unsigned int c_gcd(unsigned int u, unsigned int v);
-void *cz_make(unsigned int maxb, unsigned int maxp, uint32_t mod);
-void cz_free(void *czp);
-uint32_t cz_get(void *czp, int c, int z);
-bool cz_exists(void *czp, uint32_t val);
-struct point { int a, x, b, y; };
-void *axby_make(unsigned int maxb, unsigned int maxp, int a);
-bool axby_next(void *axbyp, struct point *pp);
-void axby_free(void *axbyp);
-''')
+import beal
 
 # run fast tests?
 _FAST = False
@@ -27,7 +12,7 @@ if os.environ.get("FAST") == "1":
 class TestModPow(unittest.TestCase):
     def __check(self, b, e, m):
         value1 = pow(b, e, m)
-        value2 = libbeal.c_modpow(b, e, m)
+        value2 = beal.modpow(b, e, m)
         self.assertEqual(value1, value2,
                 "%d != %d: %d %d %d" % (value1, value2, b, e, m))
 
@@ -58,7 +43,7 @@ class TestModPow(unittest.TestCase):
 class TestGCD(unittest.TestCase):
     def __check(self, u, v):
         value1 = fractions.gcd(u, v)
-        value2 = libbeal.c_gcd(u, v)
+        value2 = beal.gcd(u, v)
         self.assertEqual(value1, value2, "%u %u" % (u, v))
 
     def test_dense(self):
@@ -78,20 +63,20 @@ class TestGCD(unittest.TestCase):
 
 class TestCz(unittest.TestCase):
     def __check(self, maxb, maxp, mod, fast=False):
-        czp = libbeal.cz_make(maxb, maxp, mod)
+        cz = beal.cz(maxb, maxp, mod)
 
         values = set()
         for c in xrange(1, maxb+1):
             for z in xrange(3, maxp+1):
                 value1 = pow(c, z, mod)
-                value2 = libbeal.cz_get(czp, c, z)
+                value2 = cz.get(c, z)
                 self.assertEqual(value1, value2)
                 values.add(value2)
         assert len(values) > 0
 
         # assert that every value in c^z is set in the exists set
         for value in values:
-            exists = libbeal.cz_exists(czp, value)
+            exists = cz.exists(value)
             self.assertTrue(exists)
 
         # iterating over all 2^32 positions in the exists set is expensive so
@@ -101,11 +86,11 @@ class TestCz(unittest.TestCase):
         limit = 10**6 if fast else 10**8
         for _ in xrange(limit):
             value = random.randint(0, 2**32-1)
-            exists = libbeal.cz_exists(czp, value)
+            exists = cz.exists(value)
             if exists:
                 self.assertTrue(value in values)
 
-        libbeal.cz_free(czp)
+        cz.cleanup()
 
     def test_random(self):
         limit = 2 if _FAST else 40
@@ -134,24 +119,23 @@ class TestAxby(unittest.TestCase):
     def test_all_points(self):
         maxb = 200
         maxp = 200
-        point = ffi.new('struct point [1]')
         for a in xrange(1, maxb+1):
-            p = libbeal.axby_make(maxb, maxp, a)
+            axby = beal.axby(maxb, maxp, a)
             for b in xrange(1, a+1):
                 if fractions.gcd(a, b) > 1:
                     continue
                 for x in xrange(3, maxp+1):
                     for y in xrange(3, maxp+1):
-                        done = libbeal.axby_next(p, point)
+                        done, point = axby.next()
                         assert(not done)
-                        self.assertEqual(a, point[0].a)
-                        self.assertEqual(x, point[0].x)
-                        self.assertEqual(b, point[0].b)
-                        self.assertEqual(y, point[0].y)
-            done = libbeal.axby_next(p, point)
+                        aa, xx, bb, yy = point
+                        self.assertEqual(a, aa)
+                        self.assertEqual(x, xx)
+                        self.assertEqual(b, bb)
+                        self.assertEqual(y, yy)
+            done, point = axby.next()
             assert(done)
-            libbeal.axby_free(p)
-
+            axby.cleanup()
 
 if __name__ == '__main__':
     unittest.main()
