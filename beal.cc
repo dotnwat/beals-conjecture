33a8e969bbf88d862d32cd0aa59df2ff1bed7b50
Noah Watkins <noahwatkins@gmail.com>
2014-11-07 20:42:11 -0800

    cleanup: move gcd/modpow to math.h
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- beal.cc
+++ beal.cc
@@ -1,69 +1,8 @@
 #include <stdint.h>
 #include <assert.h>
+#include <unistd.h>
 #include <vector>
-
-/*
- * http://en.wikipedia.org/wiki/Modular_exponentiation
- */
-static inline uint32_t modpow(uint64_t base, uint64_t exponent, uint32_t mod)
-{
-  uint64_t result = 1;
-
-  base = base % mod;
-  while (exponent > 0) {
-    if (exponent % 2 == 1)
-      result = (result * base) % mod;
-    exponent = exponent >> 1;
-    base = (base * base) % mod;
-  }
-
-  assert(result <= (((1ULL) << 32)-1));
-  return result;
-}
-
-/*
- * http://en.wikipedia.org/wiki/Binary_GCD_algorithm
- */
-static inline unsigned int gcd(unsigned int u, unsigned int v)
-{
-  int shift;
-
-  /* GCD(0,v) == v; GCD(u,0) == u, GCD(0,0) == 0 */
-  if (u == 0)
-    return v;
-  if (v == 0)
-    return u;
-
-  /* Let shift := lg K, where K is the greatest power of 2
-        dividing both u and v. */
-  for (shift = 0; ((u | v) & 1) == 0; ++shift) {
-         u >>= 1;
-         v >>= 1;
-  }
-
-  while ((u & 1) == 0)
-    u >>= 1;
-
-  /* From here on, u is always odd. */
-  do {
-       /* remove all factors of 2 in v -- they are not common */
-       /*   note: v is not zero, so while will terminate */
-       while ((v & 1) == 0)  /* Loop X */
-           v >>= 1;
-
-       /* Now u and v are both odd. Swap if necessary so u <= v,
-          then set v = v - u (which is even). For bignums, the
-          swapping is just pointer movement, and the subtraction
-          can be done in-place. */
-       if (u > v) {
-         unsigned int t = v; v = u; u = t;
-       }  // Swap u and v.
-       v = v - u;                       // Here v >= u.
-     } while (v != 0);
-
-  /* restore common factors of 2 */
-  return u << shift;
-}
+#include "math.h"
 
 /*
  * Utility over c^z space for a particular mod value.
@@ -190,7 +129,6 @@ class work {
   work(int maxb, int maxp, uint32_t *primes, size_t nprimes) :
     maxb_(maxb), maxp_(maxp) {
       for (size_t i = 0; i < nprimes; i++) {
-        std::cout << primes[i] << std::endl;
         czs_.push_back(new cz(maxb, maxp, primes[i]));
       }
   }
@@ -207,7 +145,7 @@ class work {
     axby::point& pt = pts.next(&done);
     while (!done) {
       bool found = true;
-      for (int i = 0; i < czs_.size(); i++) {
+      for (unsigned i = 0; i < czs_.size(); i++) {
         cz *czp = czs_[i];
         uint64_t ax = czp->get(pt.a, pt.x);
         uint64_t by = czp->get(pt.b, pt.y);
